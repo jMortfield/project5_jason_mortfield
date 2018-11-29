93056e1c9be28e913ba75b673b427dc112871b60
Jason <j.mortfield44@gmail.com>
2018-11-28 20:25:26 -0500

    First commit - set up some components, did ajax request with axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  // Create object with info for beers (Name, ABV, img, brewer, type, country)
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     name: name,
+  //     abv: abv,
+  //     img: img,
+  //     brewer,
+  //     type,
+  //     country
+  //   }
+  // }
+  componentDidMount() {
+    const getBeer = () => {
+      axios
+        .get("http://proxy.hackeryou.com", {
+          params: {
+            dataType: "json",
+            method: "GET",
+            reqUrl: "http://ontariobeerapi.ca/beers",
+            xmlToJSON: false,
+            useCache: false
+          }
+        })
+        .then(res => {
+          const beerObjects = res.data
+          
+          console.log(beerObjects);
+
+        });
+    }
+    getBeer();
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+    return <div className="App">
+        <header>
+          <h1>Beer App</h1>
+          <form action="">
+            <select name="" id="">
+              <option value="name">Name</option>
+              <option value="type">Type</option>
+              <option value="abv">ABV</option>
+              <option value="country">Country</option>
+            </select>
+            <input type="text"/>
+          </form>
         </header>
-      </div>
-    );
+      </div>;
   }
 }
 
